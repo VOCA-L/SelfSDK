57e37b77ec5de8fc6f72220c5b9cb9fcc1085f74
cwlee <lcw0115a@gmail.com>
2024-02-15 15:09:17 +0900

    [cmake] so file / exe file 생성 - 현재 build 밑에 생김 - 경로 수정 예정 - 헤더 파일 오류 수정 예정

--- components/geometry/include/point.hpp
+++ components/geometry/include/point.hpp
@@ -1,57 +1,79 @@
 #ifndef SELF_SDK_GEOMETRY_POINT_H
 #define SELF_SDK_GEOMETRY_POINT_H
 
-// #include "definition.hpp"
+// #include "config.h"
 
 namespace sdk {
 namespace geometry {
 
 // To-Do
-// Add concet for using NUMBER (e.g. int, float...)
-
-template <typename T>
-class BasePoint
-{
-};
+// Add concept for using NUMBER (e.g. int, float...)
 
 /**
  * 2D Coordinate Point
  */
-template <typename T>
+template <typename P>
 class Point2D
 {
  private:
-  T x_;
-  T y_;
+  P x_;
+  P y_;
 
  public:
-  Point2D(T x, T y) : x_(x), y_(y){};
+  Point2D(P x, P y) : x_(x), y_(y){};
+
+  bool operator==(const Point2D& target) const
+  {
+    return (x_ == target.x_ && y_ == target.y_);
+  }
+  bool operator!=(const Point2D& target) const {
+    return (x_ != target.x_ || y_ != target.y_);
+  }
 };
 
 /**
  * 3D Coordinate Point
  */
-template <typename T>
+template <typename P>
 class Point3D
 {
  private:
-  T x_;
-  T y_;
-  T z_;
+  P x_;
+  P y_;
+  P z_;
 
  public:
-  Point3D(T x, T y, T z) : x_(x), y_(y), z_(z){};
+  Point3D(P x, P y, P z) : x_(x), y_(y), z_(z){}
+
+  bool operator==(const Point3D& target) const
+  {
+    return (x_ == target.x_ && y_ == target.y_ && z_ == target.z_);
+  }
+  bool operator!=(const Point3D& target) const {
+    return (x_ != target.x_ || y_ != target.y_ || z_ == target.z_);
+  }
 };
 
-template <typename T>
-class PolarPoint
+/*
+ * Polar 2D Coordiante Point
+ */
+template <typename PP>
+class PolarPoint2D
 {
  private:
-  T distance_;
-  T range_;
+  PP distance_;
+  PP range_;
 
  public:
-  PolarPoint(T dist, T r) : distance_(dist), range_(r){};
+  PolarPoint2D(PP dist, PP r) : distance_(dist), range_(r){}
+  
+  bool operator==(const PolarPoint2D& target) const
+  {
+    return (distance_ == target.distance_ && range_ == target.range_);
+  }
+  bool operator!=(const PolarPoint2D& target) const {
+    return (distance_ != target.distance_ || range_ != target.range_);
+  }
 };
 
 }  // namespace geometry
