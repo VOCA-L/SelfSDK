90cb66543ea08063b2e5b8493d97979c616c449d
cwlee <lcw0115a@gmail.com>
2026-02-22 19:49:46 +0900

    refac. Lint the code

--- test/utility/main.cpp
+++ test/utility/main.cpp
@@ -1,28 +1,27 @@
-#include "sdk-def.h"
-#include "utility/crc/include/crc-def.h"
 #include "utility/crc/include/crc16.h"
 #include "utility/crc/include/crc8.h"
 
 #include <stdio.h>
 
 int main() {
-  byte_t data[] = {0x01, 0x02, 0x03, 0x04, 0x05};
+  byte_t data[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09 };
 
   /* CRC8 Test Logic */
   CRC8_t autosar_crc8_ctx;
-  setCRC8(&autosar_crc8_ctx, CRC8_POLY_AUTOSAR, CRC8_INIT_AUTOSAR, false, false,
-          0x00);
+  setCRC8(&autosar_crc8_ctx, CRC8_POLY_AUTOSAR, CRC8_INIT_AUTOSAR, 
+      false, false, 0x00);
   byte_t crc8 = calculateCRC8(&autosar_crc8_ctx, data, sizeof(data));
 
   printf("CRC8 (AUTOSAR): 0x%02X\n", crc8);
 
   /* CRC16 Test Logic */
   CRC16_t xmodem_crc16_ctx;
-  setCRC16(&xmodem_crc16_ctx, CRC16_POLY_XMODEM, 0x0000, false, false, 0x0000);
+  setCRC16(&xmodem_crc16_ctx, CRC16_POLY_XMODEM, CRC16_INIT_XMODEM, 
+      false, false, 0x0000);
 
-  word_t crc = calculateCRC16(&xmodem_crc16_ctx, data, sizeof(data));
+  word_t crc16 = calculateCRC16(&xmodem_crc16_ctx, data, sizeof(data));
 
-  printf("CRC16 (XMODEM): 0x%04X\n", crc);
+  printf("CRC16 (XMODEM): 0x%04X\n", crc16);
 
   return 0;
 }
